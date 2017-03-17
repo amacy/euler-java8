34e15a24207a743c55c19a87aaee2a4737f815ee
Aaron Macy <aaronmacy@gmail.com>
2017-03-17 14:17:20 -0700

    059: first attempt

---
+++ src/test/java/euler/Problem59Test.java
@@ -0,0 +1,11 @@
+package euler;
+
+import org.junit.Test;
+import static junit.framework.TestCase.assertEquals;
+
+public class Problem59Test {
+  @Test
+  public void solve() {
+    assertEquals(2, Problem059.solve());
+  }
+}
